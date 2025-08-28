1246736adfa59b9d1ebdc88f0aadc61235f4a76d
lesia_onyshchuk <lkane443@gmail.com>
2025-08-29 01:57:24 +0300

    global css

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ReadingPage from "./pages/ReadingPage/ReadingPage.jsx";
 import PrivateRoute from "./PrivateRoute.jsx";
 // import Header from "./components/Header/Header.jsx";
 import { useSelector } from "react-redux";
+// import "./App.css";
 
 export default function App() {
   const { token } = useSelector((s) => s.auth);
